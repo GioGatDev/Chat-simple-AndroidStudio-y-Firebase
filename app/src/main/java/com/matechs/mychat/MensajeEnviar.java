3b5503ed39ea9720d09226369eb4f2a269a5a3b3
Giovanni Silva <giovannigatfut@gmail.com>
2018-08-28 00:30:22 -0500

    Agregando codigo
    Código de chat simple en AndroidStudio

---
+++ app/src/main/java/com/matechs/mychat/MensajeEnviar.java
@@ -0,0 +1,36 @@
+package com.matechs.mychat;
+
+import java.util.Map;
+
+/**
+ * Created by giovannimacbookpro on 29/09/17.
+ */
+
+public class MensajeEnviar extends Mensaje {
+    private Map hora;
+
+    public MensajeEnviar() {
+    }
+
+    public MensajeEnviar(Map hora) {
+        this.hora = hora;
+    }
+
+    public MensajeEnviar(String mensaje, String nombre, String fotoPerfil, String type_mensaje, Map hora) {
+        super(mensaje, nombre, fotoPerfil, type_mensaje);
+        this.hora = hora;
+    }
+
+    public MensajeEnviar(String mensaje, String urlFoto, String nombre, String fotoPerfil, String type_mensaje, Map hora) {
+        super(mensaje, urlFoto, nombre, fotoPerfil, type_mensaje);
+        this.hora = hora;
+    }
+
+    public Map getHora() {
+        return hora;
+    }
+
+    public void setHora(Map hora) {
+        this.hora = hora;
+    }
+}
